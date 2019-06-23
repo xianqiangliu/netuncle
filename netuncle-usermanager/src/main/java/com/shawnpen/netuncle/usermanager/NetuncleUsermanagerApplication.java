1d6a567173fbe5840b85ba0ae219203dcfb9ddc5
xianqiang <liuxianqiang@aliyun.com>
2019-06-23 23:14:06 +0800

    create netuncle-usermanager

---
+++ netuncle-usermanager/src/main/java/com/shawnpen/netuncle/usermanager/NetuncleUsermanagerApplication.java
@@ -0,0 +1,13 @@
+package com.shawnpen.netuncle.usermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetuncleUsermanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetuncleUsermanagerApplication.class, args);
+    }
+
+}
---
+++ netuncle-usermanager/src/test/java/com/shawnpen/netuncle/usermanager/NetuncleUsermanagerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.shawnpen.netuncle.usermanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NetuncleUsermanagerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
